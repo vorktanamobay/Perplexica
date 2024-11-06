75d7d65adaad78110545f70322b96bf62e0aab2c
Patrick Wiltrout <patrick.wiltrout@gmail.com>
2024-11-06 14:46:54 -0500

    small fixes
    - added a missing dependency to the ui package.json
    - switched chat to default to balanced mode instead of fast mode. Need to look into saving this into the db maybe, similar to how the theme is saved when selected.

--- ui/components/ChatWindow.tsx
+++ ui/components/ChatWindow.tsx
@@ -282,7 +282,7 @@ const ChatWindow = ({ id }: { id?: string }) => {
   const [messages, setMessages] = useState<Message[]>([]);
 
   const [focusMode, setFocusMode] = useState('webSearch');
-  const [optimizationMode, setOptimizationMode] = useState('speed');
+  const [optimizationMode, setOptimizationMode] = useState('balanced');
 
   const [isMessagesLoaded, setIsMessagesLoaded] = useState(false);
 
